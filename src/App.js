219888892bf7df1149f61544f241c17d050feb09
hugo-netuno <hugo.urano@gmail.com>
2024-10-07 10:40:24 -0300

    edited app js file to import login component and import custom hook, added logic to render login component, added useState hook

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Dashboard from './components/Dashboard';
+import Login from './components/Login/Login';
+import useToken from './hooks/useToken';
 
 function App() {
+  const { token, setToken } = useToken();
+
+  if(!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="wrapper">
       <h1>Application</h1>
