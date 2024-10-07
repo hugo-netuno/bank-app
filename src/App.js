a1a8006a7e84d5bc7fdd2b21b0763874a19482d8
hugo-netuno <hugo.urano@gmail.com>
2024-10-07 13:35:01 -0300

    edited component name Switch to Routes as I've learned in stack-overflow it changed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import './App.css';
 
-import Dashboard from './components/Dashboard';
+import Dashboard from './components/Dashboard/Dashboard';
 import Login from './components/Login/Login';
 import useToken from './hooks/useToken';
 
@@ -19,11 +21,11 @@ function App() {
     <div className="wrapper">
       <h1>Application</h1>
       <BrowserRouter>
-        <Switch>
+        <Routes>
           <Route path="/dashboard">
             <Dashboard />
           </Route>
-        </Switch>
+        </Routes>
       </BrowserRouter>
     </div>
   );
