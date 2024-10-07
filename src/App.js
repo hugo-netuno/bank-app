f6732b5b77a20c893e7c509e9dd00b1d00aa6d72
hugo-netuno <hugo.urano@gmail.com>
2024-10-07 10:18:23 -0300

    edited app css file, edited app js file, app imports component and renders it, app imports router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+// https://www.digitalocean.com/community/tutorials/how-to-add-login-authentication-to-react-applications
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+
+import Dashboard from './components/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
